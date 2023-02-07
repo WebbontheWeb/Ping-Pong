19867d8a6a00e6c4f244c4a1c228ff0edf76a776
Chris Webb <93842126+WebbontheWeb@users.noreply.github.com>
2023-02-06 23:51:29 -0800

    update pong

--- Assets/Scripts/BallController.cs
+++ Assets/Scripts/BallController.cs
@@ -3,11 +3,20 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class BallController : MonoBehaviour
-{
+{   
+    private Vector3 shrinkPaddle;
+    private int rightScore = 0;
+    private int leftScore = 0;
+    public Rigidbody leftPaddle; //I'm not entirely sure about this method
+    public Rigidbody rightPaddle;
+
     // Start is called before the first frame update
     void Start()
-    {
-        
+    {   
+        //WaitForSeconds
+        Rigidbody rb = GetComponent<Rigidbody>();
+        Vector3 force = Vector3.back * 1000;
+        rb.AddForce(force, ForceMode.Force);
     }
 
     // Update is called once per frame
@@ -15,4 +24,102 @@ public class BallController : MonoBehaviour
     {
         
     }
+    private void OnCollisionEnter(Collision collision)
+    {   
+        //could be cleaned up
+        // if (collision.gameObject.name == "Right Paddle")
+        // {   
+        //     BoxCollider bc = collision.gameObject.GetComponent<BoxCollider>();
+        //     Bounds bounds = bc.bounds;
+        //     float maxX = bounds.max.x;
+        //     float minX = bounds.min.x;
+
+        //     Quaternion rotation = Quaternion.Euler( 0f, 0f, 60f * (collision.gameObject.transform.position.x-minX/maxX-minX));
+            
+        //     Vector3 bounceDirection = rotation * Vector3.forward;
+
+        //     Rigidbody rb = collision.gameObject.GetComponent<Rigidbody>();
+        //     rb.AddForce(bounceDirection * 2000f, ForceMode.Force);
+        //     shrinkPaddle = new Vector3(-1f, 0.0f, 0.0f);
+        //     collision.gameObject.transform.localScale += shrinkPaddle;
+        // }  else if (collision.gameObject.name == "Left Paddle"){
+        //     BoxCollider bc = collision.gameObject.GetComponent<BoxCollider>();
+        //     Bounds bounds = bc.bounds;
+        //     float maxX = bounds.max.x;
+        //     float minX = bounds.min.x;
+
+        //     Quaternion rotation = Quaternion.Euler(60f, 60f, 60f);
+            
+        //     Vector3 bounceDirection = rotation * Vector3.back;
+
+        //     Rigidbody rb = collision.gameObject.GetComponent<Rigidbody>();
+        //     rb.AddForce(bounceDirection * 2000f, ForceMode.Force);
+        //     shrinkPaddle = new Vector3(-1f, 0.0f, 0.0f);
+        //     collision.gameObject.transform.localScale += shrinkPaddle;
+        // }
+
+        //there's gotta be a better way to do this, but I have no clue what
+        //it might be
+        Rigidbody rb = GetComponent<Rigidbody>();
+        Quaternion rotation = Quaternion.Euler(0f, 0f, 0f);
+        Vector3 force = Vector3.forward;
+
+        if (collision.gameObject.name == "Upper Wall"){
+            force = Vector3.left;
+            if(rb.velocity.z > 0){
+                rotation = Quaternion.Euler(0f, 45f, 0f);
+            } else {
+                rotation = Quaternion.Euler(0f, -45f, 0f);
+            }
+            rb.AddForce((rotation * force) * 750f, ForceMode.Force);
+        } else if(collision.gameObject.name == "Lower Wall"){
+            force = Vector3.right;
+            if(rb.velocity.z > 0){
+                rotation = Quaternion.Euler(0f, -45f, 0f);
+            } else {
+                rotation = Quaternion.Euler(0f, 45f, 0f);
+            }
+            rb.AddForce((rotation * force) * 750f, ForceMode.Force);
+        } else if(collision.gameObject.name == "Left Wall"){
+            rightScore++;
+            Debug.Log($"Right Scores! {leftScore}-{rightScore}");
+            rb.transform.position = new Vector3(0f, 0f, 0f);
+
+            rightPaddle.transform.localScale = new Vector3(8f, 1f, 1f);
+            leftPaddle.transform.localScale = new Vector3(8f, 1f, 1f);
+
+            force = Vector3.back * 500;
+            rb.AddForce(force, ForceMode.Force);
+        } else if(collision.gameObject.name == "Right Wall"){
+            leftScore++;
+            Debug.Log($"Left Scores! {leftScore}-{rightScore}");
+            rb.transform.position = new Vector3(0f, 0f, 0f);
+
+            rightPaddle.transform.localScale = new Vector3(8f, 1f, 1f);
+            leftPaddle.transform.localScale = new Vector3(8f, 1f, 1f);
+
+            force = Vector3.forward * 500;
+            rb.AddForce(force, ForceMode.Force);
+        }
+
+        if(leftScore > 10){
+            Debug.Log($"Game Over, Left Paddle Wins");
+            rightScore = 0;
+            leftScore = 0;
+        } else if (rightScore > 10){
+            Debug.Log($"Game Over, Right Paddle Wins");
+            rightScore = 0;
+            leftScore = 0;
+        }
+
+        
+
+
+
+
+
+
+    }
+
+
 }
--- Assets/Scripts/PaddleController.cs
+++ Assets/Scripts/PaddleController.cs
@@ -8,6 +8,8 @@ public class PaddleController : MonoBehaviour
     // Start is called before the first frame update
     public Rigidbody leftPaddle; //I'm not entirely sure about this method
     public Rigidbody rightPaddle;
+    private Vector3 shrinkPaddle;
+
 
 
     void Start()
@@ -35,17 +37,37 @@ public class PaddleController : MonoBehaviour
 
     private void OnCollisionEnter(Collision collision)
     {   
-        // BoxCollider bc = GetComponent<BoxCollider>();
-        // Bounds bounds = bc.bounds;
-        // float maxX = bounds.max.x;
-        // float maxY = bounds.max.y;
+        BoxCollider bc = GetComponent<BoxCollider>();
+        Bounds bounds = bc.bounds;
+        float maxX = bounds.max.x;
+        float minX = bounds.min.x;
 
-        // Quaternion rotation = Quaternion.Euler(0f,0f,60f);
-        // Vector3 bounceDirection = rotation * Vector3.up;
+        // Debug.Log($"1: {(collision.gameObject.transform.position.x-minX)}");
+        // Debug.Log($"2: {(maxX-minX)}");
+        // Debug.Log($"3: {((collision.gameObject.transform.position.x-minX)/(maxX-minX))}");
+        // Debug.Log($"Works: {(((collision.gameObject.transform.position.x-minX)/(maxX-minX))-(0.5f))}");
 
+        //Quaternion rotation = Quaternion.Euler( 0f, 60f * (((collision.gameObject.transform.position.x-minX)/(maxX-minX))-(0.5f)), 0f);
 
-        // Rigidbody rb = collision.rigidbody;
-        // rb.AddForce(bounceDirection * 1000f, ForceMode.Force);
+        Vector3 bounceDirection = Vector3.forward;
+        shrinkPaddle = new Vector3(-0.5f, 0.0f, 0.0f);
+
+        if(collision.gameObject.transform.position.z > 0){
+            bounceDirection = Quaternion.Euler( 0f, -60f * (((collision.gameObject.transform.position.x-minX)/(maxX-minX))-(0.5f)), 0f) * Vector3.back;
+            if(leftPaddle.transform.localScale.x > 2){
+                leftPaddle.transform.localScale += shrinkPaddle;
+            }
+        } else if (collision.gameObject.transform.position.z < 0){
+            bounceDirection = Quaternion.Euler( 0f, 60f * (((collision.gameObject.transform.position.x-minX)/(maxX-minX))-(0.5f)), 0f) * Vector3.forward;
+
+            if(rightPaddle.transform.localScale.x > 2){
+                rightPaddle.transform.localScale += shrinkPaddle;
+            }
+        }
+
+        collision.rigidbody.AddForce(bounceDirection * 1000f, ForceMode.Force);
 
     }
+
+
 }
